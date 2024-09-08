003376d50486ccc220d973043a32f3623dc2de72
Jason Name <jnutt367@gmail.com>
2024-09-07 22:42:59 -0400

    commit for showing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial": "linear-gradient(var(-tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "linear-gradient(from 90deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },

59c0945aa2a1635aa342ae1b98015102d0d7cf7b
Jason Name <jnutt367@gmail.com>
2024-09-08 21:23:30 -0400

    commit for nre changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-5 items-center justify-between p-24 pl-2 pr-2">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 ml-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed pl-2 left-0 top-1 right-1 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Book of Enoch&nbsp;
           <code className="font-mono font-bold">Translated from the Ethiopian by R.H. Charles, 1906.</code>
